e8929b070c2da46f3d272953dd645841df576b3f
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-07-10 21:48:44 +0530

    Add files via upload

---
+++ C8 TA2.py
@@ -0,0 +1,27 @@
+def math_steps(a,b):
+    #Add A & B. Result = C
+    c = a + b
+    
+    #Multiply C with B. Result = D
+    d = c * b
+    
+    #Divide D by A. Result = E
+    e = d / a
+    
+    #Subtract E from D. Result = F
+    f = d - e
+    
+    #Answer = F. use either print() or return keyword
+    print(f)        
+
+
+
+math_steps(5,7)
+
+
+
+
+
+
+
+
